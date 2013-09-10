33a440e164ce406c142ef2df1edd1ca96dbdad85
Maik Penz <maik@phpkuh.de>
2013-09-10 12:49:57 +0200

    revert wrong assumption.

--- debug-bar-constants/class-debug-bar-constants.php
+++ debug-bar-constants/class-debug-bar-constants.php
@@ -13,7 +13,7 @@
  */
 
 // Avoid direct calls to this file
-if ( !isset( $wp_the_query ) ) {
+if ( !function_exists( 'add_action' ) ) {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit();
