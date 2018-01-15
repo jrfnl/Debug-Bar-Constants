50b1df3d911eddfe8caa39e67850dc8b9b35e8c5
jrfnl <jrfnl@users.noreply.github.com>
2018-01-15 23:37:33 +0100

    Move text domain loading to `Debug_Bar_Constants_Init` class
    ... so the text strings are available for the deactivation message.

--- class-debug-bar-constants.php
+++ class-debug-bar-constants.php
@@ -28,24 +28,12 @@ if ( ! class_exists( 'Debug_Bar_Constants' ) ) {
 		const DBC_STYLES_VERSION = '1.7.0';
 		const DBC_SCRIPT_VERSION = '1.7.0';
 
-		const DBC_NAME = 'debug-bar-constants';
-
-		/**
-		 * Whether or not an attempt has been made to load the textdomain.
-		 * If so, no need to try again.
-		 *
-		 * @var bool
-		 */
-		private static $textdomain_loaded = false;
-
 
 		/**
 		 * Constructor.
 		 */
 		public function __construct() {
 			spl_autoload_register( array( $this, 'auto_load' ) );
-
-			add_action( 'init', array( $this, 'init' ) );
 
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
@@ -86,63 +74,12 @@ if ( ! class_exists( 'Debug_Bar_Constants' ) ) {
 
 
 		/**
-		 * Add actions which are needed for both front-end and back-end functionality.
-		 *
-		 * @return void
-		 */
-		public function init() {
-			/*
-			 * Load plugin text strings.
-			 * @link http://geertdedeckere.be/article/loading-wordpress-language-files-the-right-way
-			 */
-			$this->load_textdomain( self::DBC_NAME );
-		}
-
-
-		/**
-		 * Load the plugin text strings.
-		 *
-		 * Compatible with use of the plugin in the must-use plugins directory.
-		 *
-		 * {@internal No longer needed since WP 4.6, though the language loading in
-		 * WP 4.6 only looks at the `wp-content/languages/` directory and disregards
-		 * any translations which may be included with the plugin.
-		 * This is acceptable for plugins hosted on org, especially if the plugin
-		 * is new and never shipped with it's own translations, but not when the plugin
-		 * is hosted elsewhere.
-		 * Can be removed if/when the minimum required version for this plugin is ever
-		 * upped to 4.6. The `languages` directory can be removed in that case too.
-		 * See: {@link https://core.trac.wordpress.org/ticket/34213} and
-		 * {@link https://core.trac.wordpress.org/ticket/34114} }}
-		 *
-		 * @param string $domain Text domain to load.
-		 */
-		protected function load_textdomain( $domain ) {
-			if ( function_exists( '_load_textdomain_just_in_time' ) ) {
-				return;
-			}
-
-			if ( is_textdomain_loaded( $domain ) || self::$textdomain_loaded ) {
-				return;
-			}
-
-			$lang_path = dirname( plugin_basename( __FILE__ ) ) . '/languages';
-			if ( false === strpos( __FILE__, basename( WPMU_PLUGIN_DIR ) ) ) {
-				load_plugin_textdomain( $domain, false, $lang_path );
-			} else {
-				load_muplugin_textdomain( $domain, $lang_path );
-			}
-			self::$textdomain_loaded = true;
-		}
-
-
-		/**
 		 * Enqueue js and css files.
 		 */
 		public function enqueue_scripts() {
 			$suffix = ( ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min' );
-			wp_enqueue_style( self::DBC_NAME, plugins_url( 'css/debug-bar-constants' . $suffix . '.css', __FILE__ ), array( 'debug-bar' ), self::DBC_STYLES_VERSION );
-			wp_enqueue_script( self::DBC_NAME, plugins_url( 'js/jquery.ui.totop' . $suffix . '.js', __FILE__ ), array( 'jquery' ), self::DBC_SCRIPT_VERSION, true );
+			wp_enqueue_style( Debug_Bar_Constants_Init::DBC_NAME, plugins_url( 'css/' . Debug_Bar_Constants_Init::DBC_NAME . $suffix . '.css', __FILE__ ), array( 'debug-bar' ), self::DBC_STYLES_VERSION );
+			wp_enqueue_script( Debug_Bar_Constants_Init::DBC_NAME, plugins_url( 'js/jquery.ui.totop' . $suffix . '.js', __FILE__ ), array( 'jquery' ), self::DBC_SCRIPT_VERSION, true );
 		}
 
 
